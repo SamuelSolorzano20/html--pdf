203ae7791947c45d37d791b7d480a819d764f9e7
Samuel Solorzano <sammydrummer11@gmail.com>
2022-06-19 02:00:27 -0600

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('content', {static: false}) el!: ElementRef;
+
   title = 'PrintPDF';
+
+  generatePDF() {
+    let pdf = new jsPDF('portrait','pt','a4');
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("contrato.pdf");
+      }
+    });
+  }
+
 }
